8750aaff9d061c726918fff34626203c63d594f6
Thanos-00 <thaneshhawkings24@gmail.com>
2025-01-18 23:34:19 +0530

    Add files via upload
    day1

---
+++ day1/Chrome.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float versionOfChrome = 91.0f;
+		String developerOfChrome = "Google";
+		Boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortCut = 'c';
+		
+		System.out.println("Chrome version : " + versionOfChrome);
+		System.out.println("Chrome's developer : " + developerOfChrome);
+		System.out.println("Is Chrome a Beta version : " + isBeta);
+		System.out.println("Release year : " + releaseYear);
+		System.out.println("Short cut : " + shortCut);
+	}
+
+}
---
+++ day1/Firefox.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+		char browserLogo = 'f';
+		
+	System.out.println("Browser version : " +browserVersion);
+	System.out.println("Browser Name : " +browserName);
+	System.out.println("Browser Visiblity : " +isVisible);
+	System.out.println("Release year : " +releaseYear);
+	System.out.println("Browser Logo : " +browserLogo);
+	}
+
+}
